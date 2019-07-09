32981dd7533d4a6d284d059bbc80985ea576a4c0
caesium47 <40780512+caesium47@users.noreply.github.com>
2019-07-08 20:02:52 -0400

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,32 @@
+public class HW1 { //1
+    public static void main (String args[]) {
+        System.out.println("Paradiddle flam tap");
+
+        int x = 5; //2
+
+        if (x%100==0) {
+            System.out.println("Factor of 100");
+        }
+        else {
+            System.out.println("Not a factor of 100");
+        }
+        
+        int y = 8273; //3
+        int z = 9325;
+        //hi
+
+        if (y > 9000 && y != 9999) {
+            System.out.println("Y is greater than 9000");
+        }
+        else {
+            System.out.println("Y is less than 9000 or 9999");
+        }
+
+         if (z > 9000 && z != 9999) {
+            System.out.println("Z is greater than 9000");
+        }
+        else {
+            System.out.println("Z is less than 9000 or 9999");
+        }
+    }
+}
